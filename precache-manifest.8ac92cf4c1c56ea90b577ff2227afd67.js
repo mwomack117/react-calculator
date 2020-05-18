a0c117c781722c90c8b2f461247289b5e0e5e2d0
mwomack <michael.womack84@gmail.com>
2020-05-18 11:17:55 -0700

    Updates

---
+++ precache-manifest.8ac92cf4c1c56ea90b577ff2227afd67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e557c85c666e61933e006138c7fcbf",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "8935a8eea4b4e086721b",
+    "url": "/react-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0de7ca646f54917ae174",
+    "url": "/react-calculator/static/js/2.84236935.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.84236935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8935a8eea4b4e086721b",
+    "url": "/react-calculator/static/js/main.314df63e.chunk.js"
+  },
+  {
+    "revision": "4ad8d72ddf01a440dc4b",
+    "url": "/react-calculator/static/js/runtime-main.d3b7e15a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
